66e706db7bcccbc1dfa6b491df739b01af61dc98
VTgrace <gracee@vt.edu>
2025-12-02 09:22:17 -0500

    Upload 2025 D2P1 soln

---
+++ 2025/Day_2/P1/2025_D2P1.py
@@ -0,0 +1,33 @@
+import re
+
+def funct():
+    print("hello")
+
+def main():
+    input_file = "../input.txt"
+    sum = 0
+
+    with open(input_file, "r") as file:
+        data = file.read().split(',')
+        
+        for i in data:
+            nums = re.split("-", i)
+
+            for j in range(int(nums[0]), int(nums[1]) + 1):                
+                currNum = str(j)
+                length = len(currNum)
+
+                # Ignore numbers that have an odd amount of characters
+                if (length % 2):
+                    continue
+
+                length = length // 2
+
+                if (currNum[length:] == currNum[:length]):
+                    sum += j
+
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
