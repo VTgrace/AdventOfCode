6d1d0890facedb868e362e24d037879d9d5f1bf7
VTgrace <gracee@vt.edu>
2025-12-05 09:11:48 -0500

    Add comments

--- 2025/Day_5/P1/2025_D5P1.py
+++ 2025/Day_5/P1/2025_D5P1.py
@@ -1,26 +1,25 @@
-import re
-
-def funct():
-    print("hello")
-
 def main():
     input_file = "../input.txt"
     count = 0
     ranges = []
 
     with open(input_file, "r") as file:
-        reading_input_data = True
-        for line in file:
-            if line == "\n":
-                reading_input_data = False
-                continue
+        reading_input_ranges = True
 
+        for line in file:
+            # Switches modes
+            if line == "\n":
+                reading_input_ranges= False
+                continue
+            
             line = line.strip()
 
-            if reading_input_data:
+            if reading_input_ranges:
+                # Split line based on "-" and add to ranges
                 curr_range = line.split("-")
                 ranges.append(range(int(curr_range[0]), int(curr_range[1])))
             else:
+                # Check if current number is in range
                 for i in ranges:
                     if (int(line) in i):
                         count += 1
